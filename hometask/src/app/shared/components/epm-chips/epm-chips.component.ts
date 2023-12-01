92ce772ceefceeae287cc62bd9ec700728e8fe25
Andrew Kurylyk <andronniy7@gmail.com>
2023-12-01 16:29:46 +0200

    1

--- hometask/src/app/shared/components/epm-chips/epm-chips.component.ts
+++ hometask/src/app/shared/components/epm-chips/epm-chips.component.ts
@@ -1,3 +1,24 @@
+/*import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+
+import { INTEREST_CHIPS_NAMES } from '../../../app.config';
+
+@Component({
+  selector: 'epm-chips',
+  standalone: true,
+  imports: [CommonModule, NgOptimizedImage],
+  templateUrl: './epm-chips.component.html',
+  styleUrls: ['./epm-chips.component.scss']
+})
+export class EpmChipsComponent {
+  @Input() chips!: string;
+  @Input() isEditable = false;
+  @Input() isActive = false;
+
+  @Output() editChipsStatus: EventEmitter<string> = new EventEmitter<string>();
+
+}*/
+
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 
@@ -17,4 +38,17 @@ export class EpmChipsComponent {
 
   @Output() editChipsStatus: EventEmitter<string> = new EventEmitter<string>();
 
+  deleteChip(chipToDelete: string) {
+    this.chips = this.chips.split(' ')
+        .filter((chip) => chip !== chipToDelete)
+        .join(' ');
+    this.editChipsStatus.emit(this.chips);
+  }
+
+  updateChip(updatedChip: string) {
+    this.chips = this.chips.split(' ')
+        .map((chip) => (chip === updatedChip ? chip : chip))
+        .join(' ');
+    this.editChipsStatus.emit(this.chips);
+  }
 }
